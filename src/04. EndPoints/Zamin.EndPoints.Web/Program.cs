bb3e2c80c9286b18c506c2d0e452d5fbbd52349c
60146 <60146@YAGHOUBZADEH>
2021-10-28 09:01:25 +0330

    Add Feature

--- src/04. EndPoints/Zamin.EndPoints.Web/Program.cs
+++ src/04. EndPoints/Zamin.EndPoints.Web/Program.cs
@@ -42,7 +42,6 @@ namespace Zamin.EndPoints.Web
                 Log.CloseAndFlush();
             }
         }
-
         private WebApplicationBuilder CreateHostBuilder(string[] args, params string[] appSettingFiles)
         {
             var builder = WebApplication.CreateBuilder(args);
@@ -54,5 +53,54 @@ namespace Zamin.EndPoints.Web
 
             return builder;
         }
+
+        public int Main(string[] args, Type startUp, params string[] appSettingFiles)
+        {
+
+            if (appSettingFiles == null || !appSettingFiles.Any())
+            {
+                appSettingFiles = new string[] { "appsettings.json" };
+            }
+            var configBuilder = new ConfigurationBuilder();
+            foreach (var item in appSettingFiles)
+            {
+                configBuilder.AddJsonFile(item);
+            }
+            var configuration = configBuilder.Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args, startUp, appSettingFiles).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private IHostBuilder CreateHostBuilder(string[] args, Type startUp, params string[] appSettingFiles) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((ctx, config) =>
+            {
+                foreach (var item in appSettingFiles)
+                {
+                    config.AddJsonFile(item, true);
+                }
+            })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup(startUp);
+                })
+            .UseSerilog();
     }
 }
