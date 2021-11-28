5956664e9483f5ee35053122f1aa287ac363c6bb
oroumand <ar.oroumand@gmail.com>
2021-11-28 09:56:54 +0330

    ارتقای پروژه های لایه دیتا به سی شارپ 10 اضافه کردن لایه قرارداد های به لایه های دیتا

--- src/03. Infra/Data/Zamin.Infra.Data.Sql.Queries/BaseQueryRepository.cs
+++ src/03. Infra/Data/Zamin.Infra.Data.Sql.Queries/BaseQueryRepository.cs
@@ -1,12 +1,12 @@
-﻿namespace Zamin.Infra.Data.Sql.Queries
+﻿using Zamin.Core.Contracts.Data.Queries;
+
+namespace Zamin.Infra.Data.Sql.Queries;
+public class BaseQueryRepository<TDbContext> : IQueryRepository
+    where TDbContext : BaseQueryDbContext
 {
-    public class BaseQueryRepository<TDbContext> : IQueryRepository
-        where TDbContext : BaseQueryDbContext
+    protected readonly TDbContext _dbContext;
+    public BaseQueryRepository(TDbContext dbContext)
     {
-        protected readonly TDbContext _dbContext;
-        public BaseQueryRepository(TDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
+        _dbContext = dbContext;
     }
 }
