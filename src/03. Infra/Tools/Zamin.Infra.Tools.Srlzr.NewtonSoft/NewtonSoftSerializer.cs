00c090ea9209d35159ee0980e1f1c5731655389f
oroumand <ar.oroumand@gmail.com>
2021-12-01 09:31:10 +0330

    به روز رسانی Zamin.Infra.Tools.Srlzr.NewtonSoft به C# 10

--- src/03. Infra/Tools/Zamin.Infra.Tools.Srlzr.NewtonSoft/NewtonSoftSerializer.cs
+++ src/03. Infra/Tools/Zamin.Infra.Tools.Srlzr.NewtonSoft/NewtonSoftSerializer.cs
@@ -1,21 +1,18 @@
 ﻿using Zamin.Utilities.Services.Serializers;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-using System;
 
-namespace Zamin.Infra.Tools.Srlzr.NewtonSoft
+namespace Zamin.Infra.Tools.Srlzr.NewtonSoft;
+public class NewtonSoftSerializer : IJsonSerializer
 {
-    public class NewtonSoftSerializer : IJsonSerializer
-    {
+Zamin.Infra.Tools.Srlzr.NewtonSoft
+    public TOutput Deserialize<TOutput>(string input) =>
+        string.IsNullOrWhiteSpace(input) ?
+            default : JsonConvert.DeserializeObject<TOutput>(input);
 
-        public TOutput Deserialize<TOutput>(string input) =>
-            string.IsNullOrWhiteSpace(input) ?
-                default : JsonConvert.DeserializeObject<TOutput>(input);
+    public object Deserialize(string input, Type type) =>
+        string.IsNullOrWhiteSpace(input) ?
+            null : JsonConvert.DeserializeObject(input, type);
 
-        public object Deserialize(string input, Type type) =>
-            string.IsNullOrWhiteSpace(input) ?
-                null : JsonConvert.DeserializeObject(input, type);
-
-        public string Serilize<TInput>(TInput input) => input == null ? string.Empty : JsonConvert.SerializeObject(input, new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
-    }
+    public string Serilize<TInput>(TInput input) => input == null ? string.Empty : JsonConvert.SerializeObject(input, new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
 }
