0df67b59c671fa94818a8123f878c23eb4dd45f4
Amirhossein Tayyar <AmirHossein.Tayyar@outlook.com>
2021-12-04 16:31:46 +0330

    حدف مارکر روورژن از لایه دامین تغییر روش افزودن روورژن به انتیتی با نوشتن اکستنش متود روی EntityTypeBulider<>

---
+++ src/03. Infra/Data/Zamin.Infra.Data.Sql/Extensions/RowVersionShadowProperty.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Zamin.Infra.Data.Sql.Configurations
+{
+    public static class RowVersionShadowProperty
+    {
+        public static readonly string RowVersion = nameof(RowVersion);
+
+        public static void AddRowVersionShadowProperty<TEntity>(EntityTypeBuilder<TEntity> builder) where TEntity : class
+            => builder.Property<byte[]>(RowVersion).IsRowVersion();
+    }
+}
