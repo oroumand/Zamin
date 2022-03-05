212bd359697c80536f8d0c5edddbb17e0250c5b7
Amirhossein Tayyar <AmirHossein.Tayyar@outlook.com>
2022-03-05 21:00:23 +0330

    افزودن توابع جدید به جیسون سریالایزر و پیاده سازی آن ها

--- src/01. Utilities/Zamin.Utilities/Services/Serializers/ISerializer.cs
+++ src/01. Utilities/Zamin.Utilities/Services/Serializers/ISerializer.cs
@@ -2,7 +2,12 @@
 public interface IJsonSerializer
 {
     string Serialize<TInput>(TInput input);
+
+    string Serialize<TInput, TJsonOption>(TInput input , TJsonOption jsonOption);
+
     TOutput Deserialize<TOutput>(string input);
+
+    TOutput Deserialize<TOutput, TJsonOption>(string input, TJsonOption jsonOption);
+
     object Deserialize(string input, Type type);
 }
-
--- src/03. Infra/Tools/Zamin.Infra.Tools.Srlzr.NewtonSoft/NewtonSoftSerializer.cs
+++ src/03. Infra/Tools/Zamin.Infra.Tools.Srlzr.NewtonSoft/NewtonSoftSerializer.cs
@@ -5,13 +5,42 @@ using Newtonsoft.Json.Serialization;
 namespace Zamin.Infra.Tools.Srlzr.NewtonSoft;
 public class NewtonSoftSerializer : IJsonSerializer
 {
+    public string Serialize<TInput>(TInput input)
+        => input == null ? string.Empty : JsonConvert.SerializeObject(input, new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
+
+    public string Serialize<TInput, TJsonOption>(TInput input, TJsonOption jsonOption)
+    {
+        if (typeof(TJsonOption) == typeof(JsonSerializerSettings))
+        {
+            var option = jsonOption as JsonSerializerSettings;
+
+            if (option != null)
+                return JsonConvert.SerializeObject(input, option);
+
+            return JsonConvert.SerializeObject(input);
+        }
+
+        throw new NotSupportedException("Invalid TJsonOption Type ...!");
+    }
+
     public TOutput Deserialize<TOutput>(string input) =>
-        string.IsNullOrWhiteSpace(input) ?
-            default : JsonConvert.DeserializeObject<TOutput>(input);
+        string.IsNullOrWhiteSpace(input) ? default : JsonConvert.DeserializeObject<TOutput>(input);
+
+    public TOutput Deserialize<TOutput, TJsonOption>(string input, TJsonOption jsonOption)
+    {
+        if (typeof(TJsonOption) == typeof(JsonSerializerSettings))
+        {
+            var option = jsonOption as JsonSerializerSettings;
+
+            if (option != null)
+                return JsonConvert.DeserializeObject<TOutput>(input, option);
+
+            return JsonConvert.DeserializeObject<TOutput>(input);
+        }
+
+        throw new NotSupportedException("Invalid TJsonOption Type ...!");
+    }
 
     public object Deserialize(string input, Type type) =>
-        string.IsNullOrWhiteSpace(input) ?
-            null : JsonConvert.DeserializeObject(input, type);
-
-    public string Serialize<TInput>(TInput input) => input == null ? string.Empty : JsonConvert.SerializeObject(input, new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
+        string.IsNullOrWhiteSpace(input) ? null : JsonConvert.DeserializeObject(input, type);
 }
