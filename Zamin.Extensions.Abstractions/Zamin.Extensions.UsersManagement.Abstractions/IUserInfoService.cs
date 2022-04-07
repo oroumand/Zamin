f489b2e043a71b59e6497a6ea915e9b67dd865c7
Amirhossein Tayyar <AmirHossein.Tayyar@outlook.com>
2022-04-07 13:40:05 +0430

    اصلاح نام اینترفیس مربوط به یوزر اینفو سرویس

--- Zamin.Extensions.Abstractions/Zamin.Extensions.UsersManagement.Abstractions/IUserInfoService.cs
+++ Zamin.Extensions.Abstractions/Zamin.Extensions.UsersManagement.Abstractions/IUserInfoService.cs
@@ -1,13 +1,20 @@
 ﻿namespace Zamin.Extentions.UsersManagement.Abstractions;
+
 public interface IUserInfoService
 {
     string GetUserAgent();
+
     string GetUserIp();
+
     int UserId();
 
     string GetFirstName();
+
     string GetLastName();
+
     string GetUsername();
+
     bool IsCurrentUser(string userId);
+
     bool HasAccess(string accessKey);
 }
