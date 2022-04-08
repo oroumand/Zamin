f81769cd0847bd4156f33c112672d2f147cb12da
Amirhossein Tayyar <AmirHossein.Tayyar@outlook.com>
2022-04-08 19:09:18 +0430

    افزودن ثبت لاگ

--- Zamin.Extentions.ObjectMappers.AutoMapper/Zamin.Extentions.ObjectMappers.AutoMapper/Services/AutoMapperAdapter.cs
+++ Zamin.Extentions.ObjectMappers.AutoMapper/Zamin.Extentions.ObjectMappers.AutoMapper/Services/AutoMapperAdapter.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
 using Zamin.Extentions.ObjectMappers.Abstractions;
 
 namespace Zamin.Extentions.ObjectMappers.AutoMapper.Services;
@@ -6,14 +8,22 @@ namespace Zamin.Extentions.ObjectMappers.AutoMapper.Services;
 public class AutoMapperAdapter : IMapperAdapter
 {
     private readonly IMapper _mapper;
+    private readonly ILogger<AutoMapperAdapter> _logger;
 
-    public AutoMapperAdapter(IMapper mapper)
+    public AutoMapperAdapter(IMapper mapper, ILogger<AutoMapperAdapter> logger)
     {
         _mapper = mapper;
+        _logger = logger;
+        _logger.LogInformation("AutoMapper Adapter Start working");
     }
 
     public TDestination Map<TSource, TDestination>(TSource source)
     {
+        _logger.LogTrace("AutoMapper Adapter Map {source} To {destination} with data {sourcedata}",
+                       typeof(TSource),
+                       typeof(TDestination),
+                       JsonSerializer.Serialize(source));
+
         return _mapper.Map<TDestination>(source);
     }
 }
