92e15711a1fb5517c5de637bb57e3e1ee3b77a15
Amirhossein Tayyar <AmirHossein.Tayyar@outlook.com>
2022-04-08 22:13:36 +0430

    پباده سازی انتزاع کش ادبتر به صورت اسکیول دیستربیوت

---
+++ Zamin.Extensions.Caching.Distributed.Sql/Zamin.Extensions.Caching.Distributed.Sql/Services/DistributedSqlCacheAdapter.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using System.Text;
+using System.Text.Json;
+using Zamin.Extentions.Chaching.Abstractions;
+using Zamin.Extentions.Serializers.Abstractions;
+
+namespace Zamin.Extensions.Caching.Distributed.Sql.Services;
+
+public class DistributedSqlCacheAdapter : ICacheAdapter
+{
+    private readonly IDistributedCache _cache;
+    private readonly IJsonSerializer _serializer;
+    private readonly ILogger<DistributedSqlCacheAdapter> _logger;
+
+    public DistributedSqlCacheAdapter(IDistributedCache cache, IJsonSerializer serializer, ILogger<DistributedSqlCacheAdapter> logger)
+    {
+        _cache = cache;
+        _serializer = serializer;
+        _logger = logger;
+
+        _logger.LogInformation("DistributedCache Sql Adapter Start working");
+    }
+
+    public void Add<TInput>(string key, TInput obj, DateTime? absoluteExpiration, TimeSpan? slidingExpiration)
+    {
+        _logger.LogTrace("DistributedCache Sql Adapter Cache {obj} with key : {key} " +
+                         ", with data : {data} " +
+                         ", with absoluteExpiration : {absoluteExpiration} " +
+                         ", with slidingExpiration : {slidingExpiration}",
+                         typeof(TInput),
+                         key,
+                         _serializer.Serilize(obj),
+                         absoluteExpiration.ToString(),
+                         slidingExpiration.ToString());
+
+        var option = new DistributedCacheEntryOptions
+        {
+            AbsoluteExpiration = absoluteExpiration,
+            SlidingExpiration = slidingExpiration
+        };
+
+        _cache.Set(key, Encoding.UTF8.GetBytes(_serializer.Serilize(obj)),option);
+    }
+
+    public TOutput Get<TOutput>(string key)
+    {
+        var result = _cache.GetString(key);
+
+        if (!string.IsNullOrWhiteSpace(result))
+        {
+            _logger.LogTrace("DistributedCache Sql Adapter Successful Get Cache with key : {key}", key);
+
+            _serializer.Deserialize<TOutput>(result);
+        }
+
+        _logger.LogTrace("DistributedCache Sql Adapter Failed Get Cache with key : {key}", key);
+
+        return default;
+    }
+
+    public void RemoveCache(string key)
+    {
+        _logger.LogTrace("DistributedCache Sql Adapter Remove Cache with key : {key}", key);
+
+        _cache.Remove(key);
+    }
+}
