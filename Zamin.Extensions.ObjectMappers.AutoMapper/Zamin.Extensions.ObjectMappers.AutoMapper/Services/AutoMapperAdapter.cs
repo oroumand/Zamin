d914e73b39a3bf97e15b22cfcc78c805698eb252
Amirhossein Tayyar <AmirHossein.Tayyar@outlook.com>
2022-04-09 18:40:12 +0430

    Use Zamin IJsonSerializer

--- Zamin.Extensions.ObjectMappers.AutoMapper/Zamin.Extensions.ObjectMappers.AutoMapper/Services/AutoMapperAdapter.cs
+++ Zamin.Extensions.ObjectMappers.AutoMapper/Zamin.Extensions.ObjectMappers.AutoMapper/Services/AutoMapperAdapter.cs
@@ -2,17 +2,22 @@
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
 using Zamin.Extentions.ObjectMappers.Abstractions;
+using Zamin.Extentions.Serializers.Abstractions;
 
 namespace Zamin.Extensions.ObjectMappers.AutoMapper.Services;
 
 public class AutoMapperAdapter : IMapperAdapter
 {
     private readonly IMapper _mapper;
+    private readonly IJsonSerializer _jsonSerializer;
     private readonly ILogger<AutoMapperAdapter> _logger;
 
-    public AutoMapperAdapter(IMapper mapper, ILogger<AutoMapperAdapter> logger)
+    public AutoMapperAdapter(IMapper mapper,
+                             IJsonSerializer jsonSerializer,
+                             ILogger<AutoMapperAdapter> logger)
     {
         _mapper = mapper;
+        _jsonSerializer = jsonSerializer;
         _logger = logger;
         _logger.LogInformation("AutoMapper Adapter Start working");
     }
@@ -20,9 +25,9 @@ public class AutoMapperAdapter : IMapperAdapter
     public TDestination Map<TSource, TDestination>(TSource source)
     {
         _logger.LogTrace("AutoMapper Adapter Map {source} To {destination} with data {sourcedata}",
-                       typeof(TSource),
-                       typeof(TDestination),
-                       JsonSerializer.Serialize(source));
+                         typeof(TSource),
+                         typeof(TDestination),
+                         _jsonSerializer.Serialize(source));
 
         return _mapper.Map<TDestination>(source);
     }
