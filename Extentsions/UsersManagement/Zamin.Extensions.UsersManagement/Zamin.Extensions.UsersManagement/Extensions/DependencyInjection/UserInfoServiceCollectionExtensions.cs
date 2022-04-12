8d168e2ef2edfeb5888decceb3e6556db075e226
Alireza Oroumand <ar.oroumand@gmail.com>
2022-04-12 09:39:50 +0430

    change depemdency injection namespace

--- Extentsions/UsersManagement/Zamin.Extensions.UsersManagement/Zamin.Extensions.UsersManagement/Extensions/DependencyInjection/UserInfoServiceCollectionExtensions.cs
+++ Extentsions/UsersManagement/Zamin.Extensions.UsersManagement/Zamin.Extensions.UsersManagement/Extensions/DependencyInjection/UserInfoServiceCollectionExtensions.cs
@@ -2,19 +2,22 @@
 using Zamin.Extensions.UsersManagement.Services;
 using Zamin.Extentions.UsersManagement.Abstractions;
 
-namespace Zamin.Extensions.UsersManagement.Extensions.DependencyInjection;
+namespace Zamin.Extensions.DependencyInjection;
 
 public static class UserInfoServiceCollectionExtensions
 {
-    public static IServiceCollection AddWebUserInfoService(this IServiceCollection services)
+    public static IServiceCollection AddWebUserInfoService(this IServiceCollection services,bool useFake=false)
     {
-        services.AddSingleton<IUserInfoService, WebUserInfoService>();
-        return services;
-    }
+        if(useFake)
+        {
+            services.AddSingleton<IUserInfoService, FakeUserInfoService>();
 
-    public static IServiceCollection AddFakeUserInfoService(this IServiceCollection services)
-    {
-        services.AddSingleton<IUserInfoService, FakeUserInfoService>();
+        }
+        else
+        {
+            services.AddSingleton<IUserInfoService, WebUserInfoService>();
+
+        }
         return services;
     }
 }
