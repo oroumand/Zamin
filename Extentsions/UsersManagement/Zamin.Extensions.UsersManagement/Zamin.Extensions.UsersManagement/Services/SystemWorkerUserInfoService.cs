08b7b34cf1cff4f48a39d59909aa24e98ed97cf4
Amirhossein Tayyar <AmirHossein.Tayyar@outlook.com>
2022-05-26 17:04:39 +0430

    add UserInfoService DependencyInjection For SystemWorkerUserInfoService

---
+++ Extentsions/UsersManagement/Zamin.Extensions.UsersManagement/Zamin.Extensions.UsersManagement/Services/SystemWorkerUserInfoService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Zamin.Extensions.ObjectMappers.AutoMapper.Options;
+using Zamin.Extentions.UsersManagement.Abstractions;
+
+namespace Zamin.Extensions.UsersManagement.Services;
+
+public class SystemWorkerUserInfoService : IUserInfoService
+{
+    private readonly ILogger<SystemWorkerUserInfoService> _logger;
+    private readonly SystemWorkerUserInfoOption _configuration;
+
+    public SystemWorkerUserInfoService(ILogger<SystemWorkerUserInfoService> logger, IOptions<SystemWorkerUserInfoOption> configuration)
+    {
+        _logger = logger;
+        _configuration = configuration.Value;
+
+        _logger.LogInformation("SystemWorkerUserInfo Translator Start working with Configuration {configuration}", _configuration);
+    }
+
+    public string GetUsername() => _configuration.UserName;
+
+    public string UserId() => _configuration.UserId;
+
+    public string GetUserAgent() => _configuration.UserAgent;
+
+    public string GetUserIp() => _configuration.UserIp;
+
+    public string GetFirstName() => _configuration.FirstName;
+
+    public string GetLastName() => _configuration.LastName;
+
+    public bool HasAccess(string access) => true;
+
+    public bool IsCurrentUser(string userId) => true;
+
+    public string? GetClaim(string claimType) => null;
+}
