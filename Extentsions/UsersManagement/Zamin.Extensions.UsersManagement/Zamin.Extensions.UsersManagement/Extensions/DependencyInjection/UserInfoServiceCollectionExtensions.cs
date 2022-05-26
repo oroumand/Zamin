c3eb470cf78ec8dbaf625960e7267e5aea51bedb
Amirhossein Tayyar <AmirHossein.Tayyar@outlook.com>
2022-05-26 16:34:59 +0430

    Add UserInfoService DependencyInjection For SystemWorkerUserInfoService

--- Extentsions/UsersManagement/Zamin.Extensions.UsersManagement/Zamin.Extensions.UsersManagement/Extensions/DependencyInjection/UserInfoServiceCollectionExtensions.cs
+++ Extentsions/UsersManagement/Zamin.Extensions.UsersManagement/Zamin.Extensions.UsersManagement/Extensions/DependencyInjection/UserInfoServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Zamin.Extensions.ObjectMappers.AutoMapper.Options;
 using Zamin.Extensions.UsersManagement.Services;
 using Zamin.Extentions.UsersManagement.Abstractions;
 
@@ -6,19 +8,26 @@ namespace Zamin.Extensions.DependencyInjection;
 
 public static class UserInfoServiceCollectionExtensions
 {
-    public static IServiceCollection AddZaminWebUserInfoService(this IServiceCollection services,bool useFake=false)
+    public static IServiceCollection AddZaminWebUserInfoService(this IServiceCollection services)
+        => services.AddSingleton<IUserInfoService, WebUserInfoService>();
+
+    public static IServiceCollection AddFakeZaminWebUserInfoService(this IServiceCollection services)
+        => services.AddSingleton<IUserInfoService, FakeUserInfoService>();
+
+    public static IServiceCollection AddSystemWorkerUserInfoService(this IServiceCollection services, IConfiguration configuration)
     {
-        if(useFake)
-        {
-            services.AddSingleton<IUserInfoService, FakeUserInfoService>();
+        services.AddSingleton<IUserInfoService, SystemWorkerUserInfoService>();
+        services.Configure<SystemWorkerUserInfoOption>(configuration);
+        return services;
+    }
 
-        }
-        else
-        {
-            services.AddSingleton<IUserInfoService, WebUserInfoService>();
+    public static IServiceCollection AddSystemWorkerUserInfoService(this IServiceCollection services, IConfiguration configuration, string sectionName) 
+        => services.AddSystemWorkerUserInfoService(configuration.GetSection(sectionName));
 
-        }
+    public static IServiceCollection AddSystemWorkerUserInfoService(this IServiceCollection services, Action<SystemWorkerUserInfoOption> setupAction)
+    {
+        services.AddSingleton<IUserInfoService, SystemWorkerUserInfoService>();
+        services.Configure(setupAction);
         return services;
     }
 }
-
---
+++ Extentsions/UsersManagement/Zamin.Extensions.UsersManagement/Zamin.Extensions.UsersManagement/Options/SystemWorkerUserInfoOption.cs
@@ -0,0 +1,11 @@
+﻿namespace Zamin.Extensions.ObjectMappers.AutoMapper.Options;
+
+public class SystemWorkerUserInfoOption
+{
+    public string UserId { get; set; } = "1";
+    public string UserName { get; set; } = "SystemWorker";
+    public string UserAgent { get; set; } = "1";
+    public string UserIp { get; set; } = "0.0.0.0";
+    public string FirstName { get; set; } = "SystemWorker";
+    public string LastName { get; set; } = "SystemWorker";
+}
