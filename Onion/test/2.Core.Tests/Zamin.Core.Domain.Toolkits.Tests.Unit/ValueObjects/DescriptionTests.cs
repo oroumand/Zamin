1ab9e5b7a7c4b52f739d44d7405a38919d0113af
OneDeveloper <OneDeveloper@outlook.com>
2022-06-09 17:37:38 +0430

    اضافه شدن تست های بخش Zamin.Core.Domain.Toolkits.ValueObjects در Zamin.Core.Domain.Toolkits.Tests.Unit.ValueObjects

---
+++ Onion/test/2.Core.Tests/Zamin.Core.Domain.Toolkits.Tests.Unit/ValueObjects/DescriptionTests.cs
@@ -0,0 +1,149 @@
+﻿using Zamin.Core.Domain.Exceptions;
+
+
+namespace Zamin.Core.Domain.Toolkits.Tests.Unit.ValueObjects;
+
+public class DescriptionTests
+{
+	[Fact]
+	public void ShouldBe_ReturnNullValue_When_InputIsNull()
+	{
+		//Arrange
+		string? input = null;
+
+		//Act
+		Description description = new Description(input);
+
+		//Assert
+		Assert.Null(description.Value);
+
+	}
+
+	[Fact]
+	public void ShouldBe_ReturnStringValue_When_InputValueLessThan500AndNotNull()
+	{
+		//Arrange
+		string input = "This is a short description";
+
+		//Act
+		Description description = new Description(input);
+
+		//Assert
+		Assert.IsAssignableFrom<string>(description.Value);
+	}
+
+	[Fact]
+	public void ShouldBe_ThrowInvalidValueObjectStateException_When_DescriptionAreMoreThan500Characters()
+	{
+		//Arrange
+		string char501 =
+			"asdjfhkjdsfhlksdjfhskdjfhksdfjhjksdhfjsdfhsldfhskldfjsldfhksdjfhkjdsfhkjsdfhkdsfhksdjhfksdhfksdhfdhjasdjfhkjdsfhlksdjfhskdjfhksdfjhjksdhfjsdfhsldfhskldfjsldfhksdjfhkjdsfhkjsdfhkdsfhksdjhfksdhfksdhfdhjasdjfhkjdsfhlksdjfhskdjfhksdfjhjksdhfjsdfhsldfhskldfjsldfhksdjfhkjdsfhkjsdfhkdsfhksdjhfksdhfksdhfdhjasdjfhkjdsfhlksdjfhskdjfhksdfjhjksdhfjsdfhsldfhskldfjsldfhksdjfhkjdsfhkjsdfhkdsfhksdjhfksdhfksdhfdhjasdjfhkjdsfhlksdjfhskdjfhksdfjhjksdhfjsdfhsldfhskldfjsldfhksdjfhkjdsfhkjsdfhkdsfhksdjhfksdhfksdhfdhj1";
+
+		//Act
+		void CreateDescriptionWith501Characters()
+		{
+			Description description = new(char501);
+		}
+
+		//Assert
+		Assert.Throws<InvalidValueObjectStateException>(CreateDescriptionWith501Characters);
+	}
+
+	[Theory]
+	[InlineData("")]
+	[InlineData(null)]
+	[InlineData("Hello World")]
+	public void ShouldBe_ReturnDescriptionObject_When_CallFromStringStaticMethod(string input)
+	{
+		//Arrange
+		string stringDescription = input;
+
+		//Act
+		var fromString = Description.FromString(stringDescription);
+
+		//Assert
+		Assert.IsType<Description>(fromString);
+		Assert.Equal(stringDescription, fromString.Value);
+	}
+
+	[Theory]
+	[InlineData("", "")]
+	[InlineData(null, null)]
+	[InlineData("same", "same")]
+	public void ShouldBe_TwoDescriptionObjectsAreEqual_When_TheyHaveSameValue(string input1, string input2)
+	{
+		//Arrange
+		Description description1 = new(input1);
+		Description description2 = new(input2);
+
+		//Act
+		var result1 = description1 == description2;
+		//var result2 = description1.Value==description2.Value; //Test Failed for null input
+		var result3 = description1.Equals(description2);
+		//var result4 = description1.Value.Equals(description2.Value); //Test Failed for null input
+
+		//Arrange
+		Assert.True(result1);
+		//Assert.True(result2);
+		Assert.True(result3);
+		//Assert.True(result4);
+	}
+
+	[Fact]
+	public void ShouldBe_StringCastToDescriptionObject_When_UseImplicitCast()
+	{
+		//Arrange
+		string hello = "hello";
+
+
+		//Act
+		Description descriptionObject = hello;
+
+		//Assert
+		Assert.IsType<Description>(descriptionObject);
+		Assert.Equal(hello, descriptionObject);
+		Assert.Equal(hello, descriptionObject.Value);
+		Assert.Equal(hello, descriptionObject.Value.ToString());
+
+	}
+	[Fact]
+	public void ShouldBe_StringCastToDescriptionObject_When_UseExplicitCast()
+	{
+		//Arrange
+		string hello = "hello";
+
+		Description descriptionObject = new(hello);
+
+		//Act
+		var description = (Description)hello;
+
+		bool equalityCheck1 = description.Equals(descriptionObject);
+		bool equalityCheck2 = description == descriptionObject;
+		bool equalityCheck3 = description.Value == descriptionObject.Value;
+
+		//Assert
+		Assert.IsType<Description>(description);
+		Assert.Equal(hello, description.Value);
+		Assert.Equal(hello, description.Value.ToString());
+
+		Assert.True(equalityCheck1);
+		Assert.True(equalityCheck2);
+		Assert.True(equalityCheck3);
+
+	}
+
+	[Fact]
+	public void ShouldBe_DescriptionObjectCastToString_When_ExplicitCast()
+	{
+		//Arrange
+		Description description = new("hello");
+		string str = String.Empty;
+
+		//Act
+		str = (string)description;
+
+		//Assert
+		Assert.Equal("hello", str);
+	}
+
+}
---
+++ Onion/test/2.Core.Tests/Zamin.Core.Domain.Toolkits.Tests.Unit/ValueObjects/PriorityTests.cs
@@ -0,0 +1,224 @@
+﻿using Zamin.Core.Domain.Exceptions;
+
+namespace Zamin.Core.Domain.Toolkits.Tests.Unit.ValueObjects;
+public class PriorityTests
+{
+	[Fact]
+	public void ShouldBe_CreatePriorityObject_When_ValidInput()
+	{
+		//Arrange
+		int input = 1;
+
+		//Act
+		Priority priority = new(input);
+
+		//Assert
+		Assert.Equal(input, priority);
+
+	}
+
+	[Theory]
+	[InlineData(0)]
+	[InlineData(-1)]
+	public void ShouldBe_ThrowInvalidValueObjectStateException_When_InputNumberLessThanOne(int input)
+	{
+		//Arrange
+		int inputNumber = input;
+
+		//Act
+		void CreatePriority()
+		{
+			Priority priority = new(inputNumber);
+		}
+
+		//Assert
+		Assert.Throws<InvalidValueObjectStateException>(CreatePriority);
+	}
+
+	[Fact]
+	public void ShouldBe_IncreasePriorityNumber_When_CallIncreaseMethod()
+	{
+		//Arrange
+		Priority priority = new(1);
+
+		//Act
+		var newPriority = priority.Increase(2);
+
+		//Assert
+		Assert.Equal(3, newPriority);
+	}
+
+	[Fact]
+	public void ShouldBe_IncreasePriorityNumber_When_UsePlusOperator()
+	{
+		//Arrange
+		Priority priority = new(1);
+
+		//Act
+		Priority newPriority = priority + 3;
+
+		//Arrange
+		Assert.Equal(4, newPriority);
+	}
+
+	[Fact]
+	public void ShouldBe_DecreasePriorityNumber_When_CallDecreaseMethod()
+	{
+		//Arrange
+		Priority priority = new(3);
+
+		//Act
+		var newPriority = priority.Decrease(2);
+
+		//Assert
+		Assert.Equal(1, newPriority);
+	}
+
+	[Fact]
+	public void ShouldBe_DecreasePriorityNumber_When_UseMinusOperator()
+	{
+		//Arrange
+		Priority priority = new(3);
+
+		//Act
+		var newPriority = priority - 2;
+
+		//Assert
+		Assert.Equal(1, newPriority);
+	}
+
+	[Fact]
+	public void ShouldBe_InvalidValueObjectStateException_When_CallDecreaseMethodAndResultIsLessThanOne()
+	{
+		//Arrange
+		Priority priority = new(1);
+
+		//Act
+		void DecreasePriorityLessThan1()
+		{
+			var newPriority = priority.Decrease(1);
+			//newPriority is 0
+		}
+
+		//Assert
+		Assert.Throws<InvalidValueObjectStateException>(DecreasePriorityLessThan1);
+	}
+
+	[Fact]
+	public void ShouldBe_CheckPriorityLessThan_When_UseLessThanOperator()
+	{
+		//Arrange
+		Priority priority1 = new(1);
+		Priority priority2 = new(2);
+
+		//Act
+		var lessThan = priority1 < priority2;
+
+		//Assert
+		Assert.True(lessThan);
+	}
+
+	[Fact]
+	public void ShouldBe_CheckPriorityLessThanOrEqual_When_UseLessThanOrEqualOperator()
+	{
+		//Arrange
+		Priority priority1 = new(1);
+		Priority priority2 = new(1);
+
+		//Act
+		var lessThan = priority1 <= priority2;
+
+		//Assert
+		Assert.True(lessThan);
+	}
+
+	[Fact]
+	public void ShouldBe_CheckPriorityGreaterThan_When_UseGreaterThanOperator()
+	{
+		//Arrange
+		Priority priority1 = new(2);
+		Priority priority2 = new(1);
+
+		//Act
+		var lessThan = priority1 > priority2;
+
+		//Assert
+		Assert.True(lessThan);
+	}
+	[Fact]
+	public void ShouldBe_CheckPriorityGreaterThanOrEqual_When_UseGreaterThanOrEqualOperator()
+	{
+		//Arrange
+		Priority priority1 = new(2);
+		Priority priority2 = new(1);
+
+		//Act
+		var lessThan = priority1 >= priority2;
+
+		//Assert
+		Assert.True(lessThan);
+	}
+
+	[Fact]
+	public void ShouldBe_PriorityObjectCastToInt_When_ExplicitCast()
+	{
+		//Arrange
+		int number = 0;
+		Priority priority = new Priority(1);
+
+		//Act
+		number = (int)priority;
+
+		//Assert
+		Assert.Equal(number, priority.Value);
+		Assert.Equal(1,number);
+	}
+
+	[Fact]
+	public void ShouldBe_IntCastToPriorityObject_When_ImplicitCast()
+	{
+		//Arrange
+		int number = 5;
+
+		//Act
+		Priority priority = number;
+
+		//Assert
+		Assert.Equal(number,priority.Value);
+		Assert.Equal(5,priority.Value);
+		Assert.Equal(5,priority);
+	}
+
+	[Fact]
+	public void ShouldBe_ReturnPriorityObject_When_CallFromIntStaticMethod()
+	{
+		//Arrange
+		int number = 3;
+
+		//Act
+		Priority fromInt = Priority.FromInt(number);
+		
+		//Assert
+		Assert.Equal(fromInt, number);
+		Assert.Equal(3,fromInt);
+	}
+
+	[Theory]
+	[InlineData(0)]
+	[InlineData(-1)]
+	public void ShouldBe_ThrowInvalidValueObjectStateException_When_CallFromIntStaticMethodAndNumberIsLessThan1(int inputNumber)
+	{
+		//Arrange
+		int number = inputNumber;
+
+		//Act
+		void ConvertIntToPriority()
+		{
+			Priority fromInt = Priority.FromInt(number);
+		}
+
+		//Assert
+		Assert.Throws<InvalidValueObjectStateException>(ConvertIntToPriority);
+	}
+
+}
