3c1a00a1dac111594871ebf66b073aefbde2752c
Mohammad Ghorbani <mo.ghorbani@dotin.ir>
2022-09-14 08:51:35 +0430

    ✨Add NonValidatingValidator Extension

---
+++ Onion/src/4.EndPoints/Zamin.EndPoints.Web/Extentions/NonValidatingValidator/AddNonValidateValidator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace Zamin.EndPoints.Web.Extentions.NonValidatingValidator;
+
+public static class NonValidatingValidatorExtentions
+{
+    public static IServiceCollection AddNonValidatingValidator(this IServiceCollection services)
+    {
+        var validator = services.FirstOrDefault(s => s.ServiceType == typeof(IObjectModelValidator));
+        if (validator != null)
+        {
+            services.Remove(validator);
+            services.Add(new ServiceDescriptor(typeof(IObjectModelValidator), _ => new NonValidatingValidator(), ServiceLifetime.Singleton));
+        }
+        return services;
+    }
+}
---
+++ Onion/src/4.EndPoints/Zamin.EndPoints.Web/Extentions/NonValidatingValidator/NonValidatingValidator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace Zamin.EndPoints.Web.Extentions.NonValidatingValidator;
+
+public class NonValidatingValidator : IObjectModelValidator
+{
+    public void Validate(ActionContext actionContext, ValidationStateDictionary validationState, string prefix, object model)
+    {
+        foreach (var entry in actionContext.ModelState.Values)
+            entry.ValidationState = ModelValidationState.Valid;
+
+    }
+}
