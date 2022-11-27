2bdf0885670d6ed17c302d1da4543a251f447a5c
Amirhossein Tayyar <AmirHossein.Tayyar@outlook.com>
2022-11-27 15:35:09 +0330

    Update ZaminEventIds

--- Extentsions/Zamin.Extensions.Abstractions/Zamin.Extensions.Logger.Abstractions/ZaminEventId.cs
+++ Extentsions/Zamin.Extensions.Abstractions/Zamin.Extensions.Logger.Abstractions/ZaminEventId.cs
@@ -2,6 +2,10 @@
 public class ZaminEventId
 {
     public const int PerformanceMeasurement = 1001;
-    public const int CommandValidation = 1010;
-    public const int DomainValidationException = 1011;
+
+    public const int DomainValidationException = 1010;
+
+    public const int CommandValidation = 1011;
+    public const int QueryValidation = 1012;
+    public const int EventValidation = 1013;
 }
