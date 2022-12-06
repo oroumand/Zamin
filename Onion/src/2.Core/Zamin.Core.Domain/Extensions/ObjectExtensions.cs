80007703fb0d9b6bfa604b49df359881a1c6a521
Amirhossein Tayyar <AmirHossein.Tayyar@outlook.com>
2022-12-06 10:26:32 +0330

    add allow use instance extention to object

---
+++ Onion/src/2.Core/Zamin.Core.Domain/Extensions/ObjectExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Zamin.Core.Domain.Exceptions;
+
+namespace Zamin.Utilities.Extensions;
+
+public static class ObjectExtensions
+{
+    public static void AllowUseInstance(this object obj , string message)
+    {
+        if (obj is null)
+            throw new InvalidEntityStateException(message);
+    }
+}
