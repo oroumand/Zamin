9c963c1cf54928db42998b5aa0abb4533d26cc94
Alireza Oroumand <ar.oroumand@gmail.com>
2022-12-12 14:07:48 +0330

    add IMessageInboxItemRepository

---
+++ Extentsions/Zamin.Extensions.Abstractions/Zamin.Extensions.MessageBus.Abstractions/IMessageInboxItemRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Zamin.Extensions.MessageBus.Abstractions;
+public interface IMessageInboxItemRepository
+{
+    bool AllowReceive(string messageId, string fromService);
+    void Receive(string messageId, string fromService,string payload);
+}
