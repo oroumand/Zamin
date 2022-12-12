72953ab84401c8522773c17fc24df82458c67cbd
Alireza Oroumand <ar.oroumand@gmail.com>
2022-12-12 12:01:52 +0330

    add outbox to sample application

--- Onion/sample/2.Infra/Data/MiniBlog.Infra.Data.Sql.Commands/Common/MiniblogCommandDbContext.cs
+++ Onion/sample/2.Infra/Data/MiniBlog.Infra.Data.Sql.Commands/Common/MiniblogCommandDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MiniBlog.Core.Domain.Blogs.Entities;
+using Zamin.Extensions.Events.Outbox.Dal.EF;
 using Zamin.Infra.Data.Sql.Commands;
 
 namespace MiniBlog.Infra.Data.Sql.Commands.Common
 {
-    public class MiniblogCommandDbContext : BaseCommandDbContext
+    public class MiniblogCommandDbContext : BaseOutboxCommandDbContext
     {
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Person> People { get; set; }
---
+++ Onion/sample/2.Infra/Data/MiniBlog.Infra.Data.Sql.Commands/Migrations/20221212082824_init-with-outbox.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniBlog.Infra.Data.Sql.Commands.Migrations
+{
+    public partial class initwithoutbox : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "zamin");
+
+            migrationBuilder.CreateTable(
+                name: "OutBoxEventItems",
+                schema: "zamin",
+                columns: table => new
+                {
+                    OutBoxEventItemId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AccuredByUserId = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    AccuredOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AggregateName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    AggregateTypeName = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    AggregateId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EventName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    EventTypeName = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    EventPayload = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TraceId = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    SpanId = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    IsProcessed = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutBoxEventItems", x => x.OutBoxEventItemId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OutBoxEventItems",
+                schema: "zamin");
+        }
+    }
+}
