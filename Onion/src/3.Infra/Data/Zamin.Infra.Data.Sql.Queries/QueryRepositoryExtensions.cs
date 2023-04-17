e88565bf68e3d2cd4074ad4da04acf3f14e3650f
a.oroumand <a.oroumand@dotin.ir>
2023-04-17 08:53:35 +0330

    Add QueryRepositoryExtensions

---
+++ Onion/src/3.Infra/Data/Zamin.Infra.Data.Sql.Queries/QueryRepositoryExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Zamin.Core.Contracts.ApplicationServices.Queries;
+using Zamin.Core.Contracts.Data.Queries;
+using Zamin.Utilities.Extensions;
+
+namespace Zamin.Infra.Data.Sql.Queries;
+
+public static class QueryRepositoryExtensions
+{
+    public static async Task<PagedData<TResult>> ToPagedData<TEntity, TQuery, TResult>(this IQueryable<TEntity> entities, PageQuery<PagedData<TQuery>> query, Func<TEntity, TResult> selectFunc)
+    {
+        var result = new PagedData<TResult>
+        {
+            PageNumber = query.PageNumber,
+            PageSize = query.PageSize
+        };
+        if (query.NeedTotalCount)
+            result.TotalCount = await entities.CountAsync();
+
+        if (!string.IsNullOrWhiteSpace(query.SortBy))
+            entities = entities.OrderByField(query.SortBy, query.SortAscending);
+        entities = entities.Skip(query.SkipCount).Take(query.PageSize);
+
+        result.QueryResult = await entities.Select(
+               c => selectFunc(c)).ToListAsync();
+        return result;
+    }
+}
