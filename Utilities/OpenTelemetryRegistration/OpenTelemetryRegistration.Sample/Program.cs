a4b9e217884739c8e5d3e6611cfb4ed61121a9ba
AbbasiMohamad <m.abbasi.72@gmail.com>
2023-11-24 17:29:22 +0330

    feat: add sampling support for traces

--- Utilities/OpenTelemetryRegistration/OpenTelemetryRegistration.Sample/Program.cs
+++ Utilities/OpenTelemetryRegistration/OpenTelemetryRegistration.Sample/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddZaminTraceSupport(c =>
     c.ServiceId = "cb387bb6-9a66-444f-92b2-ff64e2a81f98";
     c.OltpEndpoint = "http://localhost:4317";
     c.ExportProcessorType = OpenTelemetry.ExportProcessorType.Simple; 
+    c.SamplingProbability = 1;
 });
 var app = builder.Build();
 
