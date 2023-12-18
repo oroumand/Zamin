fb5e22cd48c26b1628810b7b20f7466221d89778
Mohamad Ghorbani <m.ghorbani@isinpc.ir>
2023-12-18 10:44:57 +0330

    Add Common Translator Keys And Values For Seed Parrot Translator

---
+++ Onion/src/1.Utilities/Zamin.Utilities/Resources/Translators/TranslatorValues.cs
@@ -0,0 +1,45 @@
+﻿namespace Zamin.Utilities.Resources.Translators;
+
+public class TranslatorValues
+{
+    #region Commom
+
+    public const string VALIDATION_ERROR_NOT_EMPTY = "{0} باید مقدار داشته باشد.";
+    public const string VALIDATION_ERROR_NOT_NULL = "{0} نباید نال باشد.";
+    public const string VALIDATION_ERROR_IS_REQUIRED = "{0} اجباری است.";
+    public const string VALIDATION_ERROR_IS_DUPLICATED = "{0} تکراری است.";
+    public const string VALIDATION_ERROR_NOT_FOUND = "{0} یافت نشد.";
+    public const string VALIDATION_ERROR_MUST_BE_UNIQUE = "{0} باید یکتا باشد";
+    public const string VALIDATION_ERROR_NOT_EXIST = "{0} وجود ندارد.";
+    public const string VALIDATION_ERROR_STRING_MAX_LENGTH = "{0} باید حداکثر {1} کاراکتر باشد";
+    public const string VALIDATION_ERROR_MIN_STRING_LENGTH = "{0} باید حداقل {1} کاراکتر باشد";
+    public const string VALIDATION_ERROR_STRING_LENGTH_BETWEEN = "{0} باید بین {2} و {1} کاراکتر باشد";
+    public const string VALIDATION_ERROR_MAX_VALUE = "مقدار {0} باید کوچکتر {1} باشد";
+    public const string VALIDATION_ERROR_MIN_VALUE = "مقدار {0} باید بزرگتر {1} باشد";
+    public const string VALIDATION_ERROR_NOT_EQUAL = "{0} برابر  {1} نیست ";
+    public const string VALIDATION_ERROR_EQUAL = "{0}باید برابر {1} باشد";
+    public const string VALIDATION_ERROR_GREATER_THAN = "{0}باید برابر {1} باشد";
+    public const string VALIDATION_ERROR_LESS_THAN = "{0}باید برابر {1} باشد";
+    public const string VALIDATION_ERROR_VALUE_BETWEEN = "{0} باید بین {2} و {1} باشد";
+    public const string VALIDATION_ERROR_NOT_ACCESS = "شما به {0} دسترسی ندارید";
+    public const string VALIDATION_ERROR_NOT_AUTHENTICATED = "شما احراظ هویت نشده اید.";
+    public const string VALIDATION_ERROR_NOT_AUTHORIZED = "دسترسی شما احراظ نشده است";
+    public const string VALIDATION_ERROR_NOT_VALID = "مقدار {0} نامعتبر است.";
+    public const string VALIDATION_ERROR_MUST_BE = "{0} باید {1} باشد.";
+    public const string VALIDATION_ERROR_MUST_NOT_BE = "{0} نباید {1} باشد.";
+    public const string VALIDATION_ERROR_MUST_HAVE = "{0} باید {1} داشته باشد.";
+    public const string VALIDATION_ERROR_MUST_HAVE_NOT = "{0} نباید {1} داشته باشد.";
+    public const string VALIDATION_ERROR_FORMAT_INCORRECT = "فرمت {0} نا معتبر است.";
+
+    public const string NAME = "نام";
+    public const string DISPLAY_NAME = "نام نمایشی";
+    public const string TITLE = "عنوان";
+    public const string PRIMARY_KEY = "کلید اصلی";
+    public const string MESSAGE = "متن پیام";
+    public const string CHILD = "فرزند";
+    public const string PARENT = "والد";
+    public const string VALUE = "مقداز";
+
+    #endregion
+
+}
