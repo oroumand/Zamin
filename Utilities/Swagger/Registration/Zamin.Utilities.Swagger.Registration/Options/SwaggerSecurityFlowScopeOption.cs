c0c371f018dc6174819a8f02ca557d12b8195d43
Amirhossein Tayyar <AmirHossein.Tayyar@outlook.com>
2023-12-24 13:11:27 +0330

    feat : Add Zamin Swagger Utilities #uitilities-swagger

---
+++ Utilities/Swagger/Registration/Zamin.Utilities.Swagger.Registration/Options/SwaggerSecurityFlowScopeOption.cs
@@ -0,0 +1,16 @@
+﻿namespace Zamin.Utilities.Swagger.Registration.Options;
+
+public class SwaggerSecurityFlowScopeOption
+{
+    public bool Enabled { get; set; } = true;
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = string.Empty;
+
+    public KeyValuePair<string, string> ToKeyValuePair(int priority)
+    {
+        if (string.IsNullOrWhiteSpace(Name))
+            throw new ArgumentNullException($"SwaggerSecurity{priority} Scope name is null.");
+
+        return new KeyValuePair<string, string>(Name, Description);
+    }
+}
