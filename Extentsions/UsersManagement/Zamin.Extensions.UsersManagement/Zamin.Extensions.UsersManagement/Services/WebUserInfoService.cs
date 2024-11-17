b2e05c733bf0855d3f965cf4892479e34978cfd8
oroumand <ar.oroumand@gmail.com>
2024-11-17 13:52:13 +0330

    build: Update to .NET 9

--- Extentsions/UsersManagement/Zamin.Extensions.UsersManagement/Zamin.Extensions.UsersManagement/Services/WebUserInfoService.cs
+++ Extentsions/UsersManagement/Zamin.Extensions.UsersManagement/Zamin.Extensions.UsersManagement/Services/WebUserInfoService.cs
@@ -1,48 +1,38 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.Options;
 using System.Security.Claims;
 using Zamin.Extensions.UsersManagement.Extensions;
 using Zamin.Extensions.UsersManagement.Options;
 using Zamin.Extensions.UsersManagement.Abstractions;
+using Microsoft.AspNetCore.Http;
 
 namespace Zamin.Extensions.UsersManagement.Services;
 
-public class WebUserInfoService : IUserInfoService
+public class WebUserInfoService(IHttpContextAccessor httpContextAccessor, IOptions<UserManagementOptions> configuration) : IUserInfoService
 {
-    private readonly IHttpContextAccessor _httpContextAccessor;
-    private readonly UserManagementOptions _configuration;
-
-    public WebUserInfoService(IHttpContextAccessor httpContextAccessor, IOptions<UserManagementOptions> configuration)
-    {
-        _httpContextAccessor = httpContextAccessor;
-        _configuration = configuration.Value;
-    }
+    private readonly UserManagementOptions _configuration = configuration.Value;
 
     public string GetUserAgent()
-    => _httpContextAccessor?.HttpContext?.Request?.Headers["User-Agent"] ?? _configuration.DefaultUserAgent;
+    => httpContextAccessor?.HttpContext?.Request?.Headers["User-Agent"] ?? _configuration.DefaultUserAgent;
 
     public string GetUserIp()
-    => _httpContextAccessor.HttpContext?.Connection?.RemoteIpAddress?.ToString() ?? _configuration.DefaultUserIp;
+    => httpContextAccessor.HttpContext?.Connection?.RemoteIpAddress?.ToString() ?? _configuration.DefaultUserIp;
 
     public string UserId()
-    => _httpContextAccessor?.HttpContext?.User?.GetClaim(ClaimTypes.NameIdentifier) ?? string.Empty;
+    => httpContextAccessor?.HttpContext?.User?.GetClaim(ClaimTypes.NameIdentifier) ?? string.Empty;
 
     public string GetUsername()
-    => _httpContextAccessor.HttpContext?.User?.GetClaim(ClaimTypes.Name) ?? _configuration.DefaultUsername;
+    => httpContextAccessor.HttpContext?.User?.GetClaim(ClaimTypes.Name) ?? _configuration.DefaultUsername;
 
     public string GetFirstName()
-    => _httpContextAccessor.HttpContext?.User?.GetClaim(ClaimTypes.GivenName) ?? _configuration.DefaultFirstName;
+    => httpContextAccessor.HttpContext?.User?.GetClaim(ClaimTypes.GivenName) ?? _configuration.DefaultFirstName;
 
     public string GetLastName()
-    => _httpContextAccessor.HttpContext?.User?.GetClaim(ClaimTypes.Surname) ?? _configuration.DefaultLastName;
+    => httpContextAccessor.HttpContext?.User?.GetClaim(ClaimTypes.Surname) ?? _configuration.DefaultLastName;
 
-    public bool IsCurrentUser(string userId)
-    {
-        return string.Equals(UserId().ToString(), userId, StringComparison.OrdinalIgnoreCase);
-    }
+    public bool IsCurrentUser(string userId) => string.Equals(UserId().ToString(), userId, StringComparison.OrdinalIgnoreCase);
 
     public string? GetClaim(string claimType)
-    => _httpContextAccessor.HttpContext?.User?.GetClaim(claimType);
+    => httpContextAccessor.HttpContext?.User?.GetClaim(claimType);
 
     public string UserIdOrDefault() => UserIdOrDefault(_configuration.DefaultUserId);
 
