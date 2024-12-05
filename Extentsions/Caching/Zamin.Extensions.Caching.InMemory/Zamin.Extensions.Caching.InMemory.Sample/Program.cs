b00b56badbd27151b7ae0be27b0948ceb4019af5
Amirhossein Tayyar <AmirHossein.Tayyar@outlook.com>
2024-12-05 15:00:30 +0330

    ⬆️ build: Migrate Zamin.Extensions.Caching.InMemory to .NET 9
    - Updated `TargetFramework` and dependencies from .NET 8 to .NET 9.
    - Installed and configured `ZaminMicrosoftSerializer` in the sample project.
    - Adjusted project files to ensure compatibility with .NET 9.

--- Extentsions/Caching/Zamin.Extensions.Caching.InMemory/Zamin.Extensions.Caching.InMemory.Sample/Program.cs
+++ Extentsions/Caching/Zamin.Extensions.Caching.InMemory/Zamin.Extensions.Caching.InMemory.Sample/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddZaminInMemoryCaching();
+builder.Services.AddZaminMicrosoftSerializer();
 
 //Middlewares
 
