f6018c7d6503243f0e9ff283bd72339ab1136c24
d.yaghooti <d.yaghooti@mail.com>
2025-04-29 16:51:49 +0330

    feat: add Zamin.Utilities.Sinks.Elasticsearch

---
+++ Utilities/Sinks/Elasticsearch/Zamin.Utilities.Sinks.Elasticsearch.Sample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zamin.Utilities.Sinks.Elasticsearch.Sample.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController(ILogger<WeatherForecastController> logger) : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        logger.LogInformation("Log from Action Body");
+
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
